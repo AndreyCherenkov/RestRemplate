d2f25336bf71e7796c021d99d35c61cca39b8000
Andrey Cherenkov <andrey.cherenkov.199661@gmail.com>
2024-06-10 23:55:42 +0700

    Дописаны тесты для репозитория жанров

--- src/test/java/ru/andreycherenkov/GenreRepositoryTest.java
+++ src/test/java/ru/andreycherenkov/GenreRepositoryTest.java
@@ -1,7 +1,6 @@
 package ru.andreycherenkov;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -9,9 +8,16 @@ import org.testcontainers.utility.DockerImageName;
 import ru.andreycherenkov.model.Genre;
 import ru.andreycherenkov.repository.impl.GenreRepository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GenreRepositoryTest {
 
     @Container
@@ -26,17 +32,85 @@ class GenreRepositoryTest {
             mysqlContainer.getPassword());
 
     @Test
+    @Order(1)
     void findAllReturnFiveGenres() {
         List<Genre> genres = genreRepository.findAll();
         assertEquals(5, genres.size());
     }
 
     @Test
+    @Order(2)
     void afterSaveGenreFindAllReturn6Genres() {
         Genre genre = new Genre();
         genre.setName("test");
         genreRepository.save(genre);
         assertEquals(6, genreRepository.findAll().size());
+    }
+
+    @Test
+    @Order(3)
+    void createNewGenre() {
+        Genre genre = new Genre();
+        genre.setName("newgenre");
+        Genre savedGenre = genreRepository.save(genre);
+        assertEquals("newgenre", savedGenre.getName());
+    }
+
+    @Test
+    @Order(4)
+    void findGenreById1() {
+        Genre genre = genreRepository.findById(1L);
+        assertEquals("фантастика", genre.getName());
+    }
+
+    @Test
+    @Order(5)
+    void afterDeleteMethodFindAllReturn6Genres() {
+        boolean bool = genreRepository.deleteById(1L);
+        assertTrue(bool);
+        assertEquals(6, genreRepository.findAll().size());
+    }
+
+    @Test
+    @Order(6)
+    void whenUpdateGenreThenIdDidntChangeAndFindAllReturn6Genres() {
+        Genre genre = genreRepository.findById(6L);
+        genre.setName("changedname");
+        genreRepository.save(genre);
+        genre = genreRepository.findById(6L);
+        assertEquals(6, genreRepository.findAll().size());
+        assertEquals(6L, genre.getId());
+        assertEquals("changedname", genre.getName());
+    }
+
+    @Test
+    @Order(7)
+    void whenFindById8ThenGenreIdAndNameAreNull() {
+        Genre genre = genreRepository.findById(8L);
+        assertNull(genre.getId());
+        assertNull(genre.getName());
+    }
+
+    @Test
+    @Order(8)
+    void whenIdIsNegativeThenGenreIsNull() {
+        Genre genre = genreRepository.findById(-1L);
+        assertNull(genre);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        try (Connection connection = DriverManager.getConnection(mysqlContainer.getJdbcUrl(),
+                mysqlContainer.getUsername(),
+                mysqlContainer.getPassword())) {
+            Statement stmt = connection.createStatement();
+            stmt.execute("DELETE FROM author_book");
+            stmt.execute("DELETE FROM books");
+            stmt.execute("DELETE FROM authors");
+            stmt.execute("DELETE FROM genres");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
